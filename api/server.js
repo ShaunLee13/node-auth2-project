e2ec17424a0d2d3d0d649ead0234a94b3addd530
Shaun Lee <shaundlee.0103@gmail.com>
2020-08-18 16:35:18 -0400

    Built endpoints and restricting middleware

--- api/server.js
+++ api/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const bcryptjs = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const constants = require('../config/constants')
 
+const Users = require('./user-model')
+const restricted = require('../restricted/middleware')
 
 const server = express();
 
@@ -14,15 +19,70 @@ server.get("/", (req, res) => {
 });
 
 server.post('/api/register', (req, res) => {
+    const user = req.body;
 
+    if (user.username && user.password && user.department) {
+      user.password = bcryptjs.hashSync(user.password, constants.rounds);
+      Users.add(user)
+        .then(user => {
+          res.status(201).json({ data: user });
+        })
+        .catch(error => {
+          res.status(500).json({ message: error.message });
+        });
+    } else {
+      res.status(400).json({
+        message: "Please provide all necessary information for registration.",
+      });
+    }
 })
 server.post('/api/login', (req, res) => {
-    
+    const { username, password } = req.body;
+
+  if (username && password) {
+    Users.findBy({ username: username })
+      .then(([user]) => {
+        if (user && bcryptjs.compareSync(password, user.password)) {
+          const token = signToken(user)
+
+          res.status(200).json({ message: "Access granted", token });
+        } else {
+          res.status(401).json({ message: "You shall not pass!" });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ message: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message: "Please provide username and password",
+    });
+  }
 })
-server.get('/api/users', (req, res) => {
-    
+server.get('/api/users', restricted, (req, res) => {
+    Users.find()
+    .then(users => {
+      res.status(200).json({users});
+    })
+    .catch(error => 
+        res.status(500).json({message: error.message}));
 })
 
 
+function signToken(user) {
+    const payload = {
+      subject: user.id,
+      username: user.username,
+      role:user.role
+    }
+  
+    const secret = constants.jwt
+  
+    const options = {
+      expiresIn: '1d'
+    }
+  
+    return jwt.sign(payload, secret, options)
+  }
 
 module.exports = server;
